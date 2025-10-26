66e1693b0ba1a0076d61c2bbf87c7a0e64caacf1
Mihai <alexmihai.ciobanu@gmail.com>
2025-10-26 19:37:50 +0000

    Add styles for .mdx files and add an image.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,10 +1,13 @@
 import type { MDXComponents } from "mdx/types";
+import Image, { ImageProps } from "next/image";
 
 export function SampleComponent({ children }: { children: React.ReactNode }) {
   return <div className="text-red-500">{children}</div>;
 }
 
 const components: MDXComponents = {
+  h2: ({ children }) => <h2 className="text-red-500">{children}</h2>,
+  img: (props) => <Image height={400} width={400} {...(props as ImageProps)} />,
   MyComponent: SampleComponent,
 };
 
