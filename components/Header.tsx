4739285f8c77f8f49224b838ebb024464e466c09
Mihai <alexmihai.ciobanu@gmail.com>
2026-01-08 20:56:30 +0000

    Fix dynamic colors in Header.

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,38 +10,36 @@ export default function Header({ className }: HeaderProps) {
   return (
     <header className={className}>
       <nav className="flex justify-between">
-        <div>
+        <div className="flex flex-col items-center">
           <Link href="/">
             <p className="font-logo text-tertiary text-2xl font-bold">
               VOIDUNDERFLOW
             </p>
           </Link>
-        </div>
-        <div>
-          <ul className="flex gap-8">
-            <li>
-              <NavLink href="/" color="primary">
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink href="/posts" color="secondary">
-                Posts
-              </NavLink>
-            </li>
-            <li>
-              <NavLink href="/projects" color="tertiary">
-                Projects
-              </NavLink>
-            </li>
-            <li>
-              <NavLink href="/about" color="neutral">
-                About
-              </NavLink>
-            </li>
-          </ul>
           <ThemeToggle />
         </div>
+        <ul className="flex gap-8">
+          <li>
+            <NavLink href="/" color="primary">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink href="/posts" color="secondary">
+              Posts
+            </NavLink>
+          </li>
+          <li>
+            <NavLink href="/projects" color="tertiary">
+              Projects
+            </NavLink>
+          </li>
+          <li>
+            <NavLink href="/about" color="neutral">
+              About
+            </NavLink>
+          </li>
+        </ul>
       </nav>
     </header>
   );
--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -10,8 +10,7 @@ export default function NavLink({ href, color, children }: NavLinkProps) {
   return (
     <Link
       href={href}
-      className={`border-b-4 border-${color} hover:bg-${color}
-        hover:text-background py-1 px-2`}
+      className={`border-b-4 hover:text-background py-1 px-2 nav-link-${color}`}
     >
       {children}
     </Link>
